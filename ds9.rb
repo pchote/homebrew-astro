2a1d82a0174144c98233bf4ce129da1fe04f3d04
Paul Chote <paul@chote.net>
2013-06-28 15:59:25 +1200

    Update ds9 formula to 7.2.

--- ds9.rb
+++ ds9.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ds9 < Formula
   homepage 'http://hea-www.harvard.edu/RD/ds9/'
-  url 'http://hea-www.harvard.edu/saord/download/ds9/darwinlion/ds9.darwinlion.7.0.2.tar.gz'
-  sha1 '073cdf2c06f7fb5f6811d8da05ce8ed77a7b3c81'
+  url 'http://hea-www.harvard.edu/RD/ds9/download/darwinmountainlion/ds9.darwinmountainlion.7.2.tar.gz'
+  sha1 '4d7d17fd619ef5aa2fc14618811207faad797d76'
 
   def install
     # ds9 requires the companion zip file to live in the same location as the binary
